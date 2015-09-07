601a0a626d6bfc4cd629b711d10baea5e8d6fdf0
camuetzel <craig_list_columbus@yahoo.com>
2015-09-07 17:23:11 -0400

    Babysitter commit

---
+++ BabySitterSystem/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.StartTimeHHTextBox = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PMRadioButton = New System.Windows.Forms.RadioButton()
+        Me.StartTimeMMTextBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.BedtimeMMTextBox = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.BedtimeHHTextBox = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.StopTimeMMTextBox = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.StopTimeHHTextBox = New System.Windows.Forms.TextBox()
+        Me.AMRadioButton = New System.Windows.Forms.RadioButton()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.TotalPaymentTextBox = New System.Windows.Forms.TextBox()
+        Me.ClearButton = New System.Windows.Forms.Button()
+        Me.CalculateButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'StartTimeHHTextBox
+        '
+        Me.StartTimeHHTextBox.Location = New System.Drawing.Point(75, 60)
+        Me.StartTimeHHTextBox.Name = "StartTimeHHTextBox"
+        Me.StartTimeHHTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.StartTimeHHTextBox.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 67)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(55, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Start Time"
+        '
+        'PMRadioButton
+        '
+        Me.PMRadioButton.AutoSize = True
+        Me.PMRadioButton.Location = New System.Drawing.Point(165, 126)
+        Me.PMRadioButton.Name = "PMRadioButton"
+        Me.PMRadioButton.Size = New System.Drawing.Size(41, 17)
+        Me.PMRadioButton.TabIndex = 7
+        Me.PMRadioButton.TabStop = True
+        Me.PMRadioButton.Text = "PM"
+        Me.PMRadioButton.UseVisualStyleBackColor = True
+        '
+        'StartTimeMMTextBox
+        '
+        Me.StartTimeMMTextBox.Location = New System.Drawing.Point(126, 60)
+        Me.StartTimeMMTextBox.Name = "StartTimeMMTextBox"
+        Me.StartTimeMMTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.StartTimeMMTextBox.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(165, 67)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "PM"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(111, 61)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(12, 16)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = ":"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(111, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(12, 16)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = ":"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(165, 98)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(23, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "PM"
+        '
+        'BedtimeMMTextBox
+        '
+        Me.BedtimeMMTextBox.Location = New System.Drawing.Point(126, 91)
+        Me.BedtimeMMTextBox.Name = "BedtimeMMTextBox"
+        Me.BedtimeMMTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.BedtimeMMTextBox.TabIndex = 4
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 98)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(45, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Bedtime"
+        '
+        'BedtimeHHTextBox
+        '
+        Me.BedtimeHHTextBox.Location = New System.Drawing.Point(75, 91)
+        Me.BedtimeHHTextBox.Name = "BedtimeHHTextBox"
+        Me.BedtimeHHTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.BedtimeHHTextBox.TabIndex = 3
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(111, 122)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(12, 16)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = ":"
+        '
+        'StopTimeMMTextBox
+        '
+        Me.StopTimeMMTextBox.Location = New System.Drawing.Point(126, 121)
+        Me.StopTimeMMTextBox.Name = "StopTimeMMTextBox"
+        Me.StopTimeMMTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.StopTimeMMTextBox.TabIndex = 6
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 128)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(55, 13)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "Stop Time"
+        '
+        'StopTimeHHTextBox
+        '
+        Me.StopTimeHHTextBox.Location = New System.Drawing.Point(75, 121)
+        Me.StopTimeHHTextBox.Name = "StopTimeHHTextBox"
+        Me.StopTimeHHTextBox.Size = New System.Drawing.Size(33, 20)
+        Me.StopTimeHHTextBox.TabIndex = 5
+        '
+        'AMRadioButton
+        '
+        Me.AMRadioButton.AutoSize = True
+        Me.AMRadioButton.Location = New System.Drawing.Point(212, 126)
+        Me.AMRadioButton.Name = "AMRadioButton"
+        Me.AMRadioButton.Size = New System.Drawing.Size(41, 17)
+        Me.AMRadioButton.TabIndex = 8
+        Me.AMRadioButton.TabStop = True
+        Me.AMRadioButton.Text = "AM"
+        Me.AMRadioButton.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(74, 44)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(23, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "HH"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(136, 44)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(25, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "MM"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(12, 19)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(60, 13)
+        Me.Label11.TabIndex = 20
+        Me.Label11.Text = "Time Entry:"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(12, 177)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(75, 13)
+        Me.Label12.TabIndex = 21
+        Me.Label12.Text = "Total Payment"
+        '
+        'TotalPaymentTextBox
+        '
+        Me.TotalPaymentTextBox.Enabled = False
+        Me.TotalPaymentTextBox.Location = New System.Drawing.Point(93, 174)
+        Me.TotalPaymentTextBox.Name = "TotalPaymentTextBox"
+        Me.TotalPaymentTextBox.Size = New System.Drawing.Size(77, 20)
+        Me.TotalPaymentTextBox.TabIndex = 22
+        '
+        'ClearButton
+        '
+        Me.ClearButton.Location = New System.Drawing.Point(22, 215)
+        Me.ClearButton.Name = "ClearButton"
+        Me.ClearButton.Size = New System.Drawing.Size(75, 23)
+        Me.ClearButton.TabIndex = 23
+        Me.ClearButton.Text = "Clear"
+        Me.ClearButton.UseVisualStyleBackColor = True
+        '
+        'CalculateButton
+        '
+        Me.CalculateButton.Location = New System.Drawing.Point(139, 215)
+        Me.CalculateButton.Name = "CalculateButton"
+        Me.CalculateButton.Size = New System.Drawing.Size(75, 23)
+        Me.CalculateButton.TabIndex = 9
+        Me.CalculateButton.Text = "Calculate"
+        Me.CalculateButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.CalculateButton)
+        Me.Controls.Add(Me.ClearButton)
+        Me.Controls.Add(Me.TotalPaymentTextBox)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.AMRadioButton)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.StopTimeMMTextBox)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.StopTimeHHTextBox)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.BedtimeMMTextBox)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.BedtimeHHTextBox)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.StartTimeMMTextBox)
+        Me.Controls.Add(Me.PMRadioButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StartTimeHHTextBox)
+        Me.Name = "Form1"
+        Me.Text = "Babysitters R Us Payment System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StartTimeHHTextBox As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents PMRadioButton As RadioButton
+    Friend WithEvents StartTimeMMTextBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents BedtimeMMTextBox As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents BedtimeHHTextBox As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents StopTimeMMTextBox As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents StopTimeHHTextBox As TextBox
+    Friend WithEvents AMRadioButton As RadioButton
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents TotalPaymentTextBox As TextBox
+    Friend WithEvents ClearButton As Button
+    Friend WithEvents CalculateButton As Button
+End Class
---
+++ BabySitterSystem/Form1.vb
@@ -0,0 +1,183 @@
+﻿'Cheryl Muetzel
+'Babysitter Pay System
+'09/04/2015
+
+Public Class Form1
+    'Declare the constant.
+    Const START_TIME_TO_BEDTIME_RATE_Decimal = 12D
+    Const BEDTIME_TO_MIDNIGHT_RATE_Decimal = 8D
+    Const MIDNIGHT_TO_STOP_TIME_RATE_Decimal = 16D
+    Const MILITARY_TIME_CONVERT_Decimal = 12D
+    Const MIDNIGHT_DECIMAL = 720D
+    Const MIDNIGHT_HH = 12D
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub StartTimeHHTextBox_TextChanged(sender As Object, e As EventArgs) Handles StartTimeHHTextBox.TextChanged
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        'Clear previous entries from the form.
+        StartTimeHHTextBox.Clear()
+        StartTimeMMTextBox.Clear()
+        BedtimeHHTextBox.Clear()
+        BedtimeMMTextBox.Clear()
+        StopTimeHHTextBox.Clear()
+        StopTimeMMTextBox.Clear()
+        TotalPaymentTextBox.Clear()
+        AMRadioButton.Checked = False
+        PMRadioButton.Checked = False
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles CalculateButton.Click
+        Dim StartTimeHHInteger, StartTimeMMInteger, BedtimeHHInteger, BedtimeMMInteger, StopTimeHHInteger, StopTimeMMInteger,
+        StartTimeAccumInteger, FirstPayTierPayAmtInteger, BedtimeAccumInteger, FirstPayTierAmtInteger, EndTimeAccumInteger, SecondPayTierAmtInteger,
+        SecondPayTierPayAmtInteger, ThirdPayTierAmtInteger, ThirdPayTierPayAmtInteger, GrandTotalPayAmtInteger As Integer
+
+        Dim ValidDataBoolean As Boolean
+
+        'Initialize variables
+        ValidDataBoolean = True
+        'PMRadioButton.Equals(False)
+        'AMRadioButton.Equals(False)
+
+        Try
+            StartTimeHHInteger = Integer.Parse(StartTimeHHTextBox.Text)
+            If StartTimeHHInteger < 5 Then
+                MessageBox.Show("Start time must be after 5 pm")
+                StartTimeHHTextBox.Focus()
+                ValidDataBoolean = False
+            End If
+            Try
+                StartTimeMMInteger = Integer.Parse(StartTimeMMTextBox.Text)
+                If StartTimeMMInteger > 59 Then
+                    MessageBox.Show("Start time minutes must be less than 60")
+                    ValidDataBoolean = False
+                    StartTimeMMTextBox.Focus()
+                End If
+                Try
+                    BedtimeHHInteger = Integer.Parse(BedtimeHHTextBox.Text)
+                    Try
+                        BedtimeMMInteger = Integer.Parse(BedtimeMMTextBox.Text)
+                        If BedtimeMMInteger > 59 Then
+                            MessageBox.Show("Bed time minutes must be less than 60")
+                            ValidDataBoolean = False
+                            BedtimeMMTextBox.Focus()
+                        End If
+
+                        Try
+                            StopTimeHHInteger = Integer.Parse(StopTimeHHTextBox.Text)
+                            If (StopTimeHHInteger > 3 And StopTimeHHInteger < 12) And AMRadioButton.Checked Then
+                                MessageBox.Show("Enter a stop time before 4 am")
+                                ValidDataBoolean = False
+                                StopTimeHHTextBox.Focus()
+                            Else
+                                If (StopTimeHHInteger = 12) And PMRadioButton.Checked Then
+                                    MessageBox.Show("Enter a stop time before 4 am")
+                                    PMRadioButton.Equals(False)
+                                    ValidDataBoolean = False
+                                    StopTimeHHTextBox.Focus()
+                                End If
+                            End If
+
+                            Try
+                                StopTimeMMInteger = Integer.Parse(StopTimeMMTextBox.Text)
+                                If StopTimeMMInteger > 59 Then
+                                    MessageBox.Show("Stop time minutes must be less than 60")
+                                    ValidDataBoolean = False
+                                    StopTimeMMTextBox.Focus()
+                                End If
+
+                                If Not PMRadioButton.Checked And Not AMRadioButton.Checked Then
+                                    MessageBox.Show("Check AM button for stop hours after 11:59 pm  or PM Button for hours before midnight")
+                                    ValidDataBoolean = False
+                                    PMRadioButton.Focus()
+                                End If
+
+
+
+
+
+                                'Calculate pay per hour:  start to bedtime=$12, bedtime to midnight=$8, midnight to end=$16
+                                'No pay for partial hours
+
+                                If ValidDataBoolean.Equals(True) Then
+                                    StartTimeAccumInteger = (StartTimeHHInteger * 60) + StartTimeMMInteger
+                                    BedtimeAccumInteger = (BedtimeHHInteger * 60) + BedtimeMMInteger
+                                    EndTimeAccumInteger = (StopTimeHHInteger * 60) + StopTimeMMInteger
+
+                                    'Calculate rate for start time to bed time
+                                    FirstPayTierAmtInteger = Math.Truncate((BedtimeAccumInteger - StartTimeAccumInteger) / 60)
+                                    FirstPayTierPayAmtInteger = FirstPayTierAmtInteger * START_TIME_TO_BEDTIME_RATE_Decimal
+                                    GrandTotalPayAmtInteger = GrandTotalPayAmtInteger + FirstPayTierPayAmtInteger
+
+                                    'Calculate rate for bedtime to midnight
+                                    If StopTimeHHInteger > 0 And StopTimeHHInteger < 4 Or StopTimeHHInteger = 12 Then
+                                        SecondPayTierAmtInteger = Math.Truncate((MIDNIGHT_DECIMAL - BedtimeAccumInteger) / 60)
+                                        SecondPayTierPayAmtInteger = SecondPayTierAmtInteger * BEDTIME_TO_MIDNIGHT_RATE_Decimal
+                                    Else
+                                        SecondPayTierAmtInteger = Math.Truncate((EndTimeAccumInteger - BedtimeAccumInteger) / 60)
+                                        SecondPayTierPayAmtInteger = SecondPayTierAmtInteger * BEDTIME_TO_MIDNIGHT_RATE_Decimal
+                                    End If
+
+                                    GrandTotalPayAmtInteger = GrandTotalPayAmtInteger + SecondPayTierPayAmtInteger
+
+                                    'Calculate rate for midnight to stop time
+                                    If StopTimeHHInteger > 0 And StopTimeHHInteger < 4 Then
+                                        EndTimeAccumInteger = EndTimeAccumInteger + MIDNIGHT_DECIMAL
+                                        ThirdPayTierAmtInteger = Math.Truncate((EndTimeAccumInteger - MIDNIGHT_DECIMAL) / 60)
+                                        ThirdPayTierPayAmtInteger = ThirdPayTierAmtInteger * MIDNIGHT_TO_STOP_TIME_RATE_Decimal
+                                    Else
+                                        If StopTimeHHInteger = 12 Then
+                                            ThirdPayTierAmtInteger = Math.Truncate((EndTimeAccumInteger - MIDNIGHT_DECIMAL) / 60)
+                                            ThirdPayTierPayAmtInteger = ThirdPayTierAmtInteger * MIDNIGHT_TO_STOP_TIME_RATE_Decimal
+                                        Else
+                                            ThirdPayTierAmtInteger = 0
+                                        End If
+                                    End If
+
+                                    GrandTotalPayAmtInteger = GrandTotalPayAmtInteger + ThirdPayTierPayAmtInteger
+
+                                    'Display the grand total payment
+                                    TotalPaymentTextBox.Text = GrandTotalPayAmtInteger.ToString("C")
+                                End If
+
+                            Catch ex As Exception
+                                MessageBox.Show("Stop time minutes must be numeric")
+                                StopTimeMMTextBox.Focus()
+                            End Try
+                        Catch ex As Exception
+                            If StopTimeHHInteger > 3 And StopTimeHHInteger < 12 And AMRadioButton.Checked Then
+                                MessageBox.Show("Enter a stop time before 4 am")
+                                StopTimeHHTextBox.Focus()
+                            End If
+                            'MessageBox.Show("Stop time hour must be numeric")
+                            'StopTimeHHTextBox.Focus()
+                        End Try
+                    Catch ex As [FormatException]
+                        MessageBox.Show("Bedtime minutes must be numeric")
+                        BedtimeMMTextBox.Focus()
+
+                    End Try
+                Catch ex As [FormatException]
+                    MessageBox.Show("Bedtime hour must be numeric")
+                    BedtimeHHTextBox.Focus()
+                End Try
+
+            Catch ex As [FormatException]
+                MessageBox.Show("Start time minutes must be numeric")
+                StartTimeMMTextBox.Focus()
+            End Try
+        Catch ex As [FormatException]
+            MessageBox.Show("Start time hour must be numeric")
+            StartTimeHHTextBox.Focus()
+
+        End Try
+    End Sub
+
+End Class
